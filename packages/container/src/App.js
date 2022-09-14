e5e06fc09d5a56c2962f7a0cbfc78c6bceec2508
eylonshm <eylonshm@gmail.com>
2022-09-14 15:23:02 +0300

    fake commit to trigger action

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp'
 export default () => {
   return (
     <div>
-      <h1>Hi there!</h1>
+      <h3>Hi there!</h3>
       <hr />
       <MarketingApp />
     </div>
