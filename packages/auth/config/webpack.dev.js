c97deb934e1b1db32c959824dfce5351ab136d77
eylonshm <eylonshm@gmail.com>
2022-09-16 11:59:25 +0300

    Update routing and add Auth

--- packages/auth/config/webpack.dev.js
+++ packages/auth/config/webpack.dev.js
@@ -6,19 +6,19 @@ const packageJson = require('../package.json')
 
 const devConfig = {
   mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8082/',
+  },
   devServer: {
-    port: 8081,
+    port: 8082,
     historyApiFallback: true,
-    output: {
-      publicPath: 'http://localhost:8081/',
-    },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'marketing',
+      name: 'auth',
       filename: 'remoteEntry.js',
       exposes: {
-        './MarketingApp': './src/bootstrap',
+        './AuthApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
     }),
