56ab12da013dcaba8cd0ffbf711a480809e68346
eylonshm <eylonshm@gmail.com>
2022-09-16 12:21:26 +0300

    Add LazyLoading for the smaller apps

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
-import MarketingApp from './components/MarketingApp'
-import AuthApp from './components/AuthApp'
-import Header from './components/Header'
+import Progress from './components/Progress';
+import Header from './components/Header';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
-})
+});
 
 export default () => {
   return (
@@ -16,12 +21,14 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Switch>
-            <Route path='/auth' component={AuthApp} />
-            <Route path='/' component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
-  )
-}
+  );
+};
